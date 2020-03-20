cbc598766748d4f2d766775d8d4ec6b56ed4b5aa
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-03-20 21:09:40 +0200

    lab4

---
+++ Laborator 4/ClassLibrary1/CustomerCF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class CustomerCF
+    {
+        public int Id { get; set; }
+        [MaxLength(40)]
+        public string Name { get; set; }
+        [MaxLength(30)]
+        public string City { get; set; }
+        public ICollection<OrderCF> Orders { get; set; }
+        protected CustomerCF() { }
+    }
+}
---
+++ Laborator 4/ClassLibrary1/ModelContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    internal class ModelContext : DbContext
+    {
+        public DbSet<PersonCF> Persons { get; set; }
+        public DbSet<CustomerCF> Customers { get; set; }
+        public DbSet<OrderCF> Orders { get; set; }
+        public DbSet<ArtistCF> Artists { get; set; }
+        public DbSet<AlbumCF> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Tiberiu\\Documents\\TestPerson.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonCF>();
+
+            modelBuilder.Entity<CustomerCF>()
+                .HasMany<OrderCF>(od => od.OrderCF)
+                .WithOne(c => CustomerCF);
+
+            modelBuilder.Entity<ArtistCF>()
+                .HasMany<AlbumCF>(al => al.AlbumCF)
+                .WithMany<ArtistCF>(ar => ar.ArtistCF);
+        }
+    }
+}
