a77b6cbfcf250f1290bb5d8e76f0a86058b60250
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-05-06 16:58:07 +0300

    Laboratorul cu Asp Razor Pages

---
+++ Asp_Cocei_Tiberiu_Rp - Laborator 10/Asp_Cocei_Tiberiu_Rp/Data/Asp_Cocei_Tiberiu_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace Asp_Cocei_Tiberiu_Rp.Data
+{
+    public class Asp_Cocei_Tiberiu_RpContext : DbContext
+    {
+        public Asp_Cocei_Tiberiu_RpContext (DbContextOptions<Asp_Cocei_Tiberiu_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
