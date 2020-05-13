e29e2e9ef249dd88a2107e6ecfb58e055a597fb2
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-05-13 17:06:08 +0300

    Laboratorul 11

---
+++ Laborator 11/Client Web/WcfWebApp/Models/CommentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WcfWebApp.Models
+{
+    [DataContract(IsReference = true)]
+    public partial class CommentDTO
+    {
+        [DataMember]
+        public int CommentId { get; set; }
+        [DataMember]
+        public string Text { get; set; }
+        [DataMember]
+        public int PostPostId { get; set; }
+        [DataMember]
+        public virtual PostDTO Post { get; set; }
+    }
+
+}
---
+++ Laborator 11/Client Web/WcfWebApp/Models/PostDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WcfWebApp.Models
+{
+    [DataContract]
+    public partial class PostDTO
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public PostDTO()
+        {
+            this.Comments = new HashSet<CommentDTO>();
+        }
+
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Domain { get; set; }
+        [DataMember]
+        public System.DateTime Date { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [DataMember]
+        public virtual ICollection<CommentDTO> Comments { get; set; }
+
+    }
+
+}
