45a0adcc3995dd2cff559d33d4a881def6978735
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-05-20 17:41:28 +0300

    Laborator 12 - Proiect 3 (nu e complet)

---
+++ Laborator 12/Proiect3/Models/PropertyDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Proiect3.Models
+{
+    public class PropertyDTO
+    {
+        public PropertyDTO()
+        {
+            this.PropertyLists = new HashSet<PropertyListDTO>();
+        }
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<PropertyListDTO> PropertyLists { get; set; }
+    }
+}
---
+++ Laborator 12/Proiect3/Models/PropertyListDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Proiect3.Models
+{
+    public class PropertyListDTO
+    {
+        public int Id { get; set; }
+
+        public int FileId { get; set; }
+
+        public int PropertyId { get; set; }
+
+        public virtual FileDTO File { get; set; }
+
+        public virtual PropertyDTO Property { get; set; }
+    }
+}
